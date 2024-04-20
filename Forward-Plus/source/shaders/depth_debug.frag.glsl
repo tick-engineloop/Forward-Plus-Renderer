e51aeaf17f94bd76235dd5b93b0cdb3ca6d83ea2
LvXiaooo <1454848644@qq.com>
2024-04-20 17:38:04 +0800

    Update depth_debug.frag.glsl

--- Forward-Plus/source/shaders/depth_debug.frag.glsl
+++ Forward-Plus/source/shaders/depth_debug.frag.glsl
@@ -7,7 +7,26 @@ out vec4 fragColor;
 
 // Need to linearize the depth because we are using the projection
 float LinearizeDepth(float depth) {
+	// ==============================================================
+	// back to NDC
+	// ==============================================================
+	// Viewport transform:
+	//
+	//			 farVal - nearVal            farVal + nearVal
+	// Z_wnd = ————————————————————Z_ndc + ————————————————————
+	//                  2                           2
+	//
+	// 当 farVal = 1.0，nearVal = 0.0 时：
+	//
+	//           Z_ndc + 1
+	// Z_wnd = ——————————————
+	//               2
+	// 那么：
+	//
+	// Z_ndc = 2 * Z_wnd - 1
+	// ==============================================================
 	float z = depth * 2.0 - 1.0;
+
 	return (2.0 * near * far) / (far + near - z * (far - near));
 }
 
