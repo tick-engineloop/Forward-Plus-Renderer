e5c4e3e3debbb52e52263b8ceda5fc6d23a93f02
LvXiaooo <1454848644@qq.com>
2024-04-21 13:42:30 +0800

    Update depth_debug.frag.glsl

--- Forward-Plus/source/shaders/depth_debug.frag.glsl
+++ Forward-Plus/source/shaders/depth_debug.frag.glsl
@@ -1,14 +1,14 @@
 #version 330 core
 
-uniform float near;
-uniform float far;
+uniform float near;	// near clipping plane in camera space
+uniform float far;	// far clipping plane in camera space
 
 out vec4 fragColor;
 
 // Need to linearize the depth because we are using the projection
 float LinearizeDepth(float depth) {
 	// ==============================================================
-	// back to NDC
+	// back to NDC from screen space
 	// ==============================================================
 	// Viewport transform:
 	//
@@ -16,7 +16,7 @@ float LinearizeDepth(float depth) {
 	// Z_wnd = ————————————————————Z_ndc + ————————————————————
 	//                  2                           2
 	//
-	// 当 farVal = 1.0，nearVal = 0.0 时：
+	// 当 glDepthRange 函数指定 farVal = 1.0，nearVal = 0.0 时（默认值，这指定的是屏幕空间深度值范围，与摄像机空间的 far 和 near 不同）：
 	//
 	//           Z_ndc + 1
 	// Z_wnd = ——————————————
@@ -27,10 +27,13 @@ float LinearizeDepth(float depth) {
 	// ==============================================================
 	float z = depth * 2.0 - 1.0;
 
+	// back to camera space from NDC
 	return (2.0 * near * far) / (far + near - z * (far - near));
 }
 
 void main() {
-	float depth = LinearizeDepth(gl_FragCoord.z) / far;
+	// 片段在摄像机空间内线性化的深度值处于 near 与 far 之间，需要归一化
+	float depth = (LinearizeDepth(gl_FragCoord.z) - near) / (far - near);
+	
 	fragColor = vec4(vec3(depth), 1.0f);
 }
